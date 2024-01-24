cb0a00c43e3fa1e56b305d903cc07e0b3ec73a4b
bincooo <bincooo@iMac.local>
2024-01-25 06:01:01 +0800

    feat: 改用yaml配置文件，拓展路径重写，请求头编辑配置化

--- api/t.go
+++ api/t.go
@@ -20,6 +20,18 @@ var (
 	JA3     = "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513-21,29-23-24,0"
 )
 
+type Mapper struct {
+	Addr   string
+	Ja3    bool
+	Routes []Route
+}
+
+type Route struct {
+	Path    string
+	Rewrite string
+	Action  []string
+}
+
 type Ja3Proxies struct {
 	path  string
 	route Route
